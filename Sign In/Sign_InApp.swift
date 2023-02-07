2f84fe9498faa0901d5efe38ba60a4b434477209
Tim Hart <thart@apple.com>
2023-02-07 13:44:22 -0500

    Initial Commit

---
+++ Sign In/Sign_InApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sign_InApp.swift
+//  Sign In
+//
+//  Created by Timothy Hart on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Sign_InApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
